0d9ba35f9653b3587c3f272f57d8a1051fa1b1ce
annesaerker <32764898+annesaerker@users.noreply.github.com>
2017-10-31 16:03:32 +0100

    chart almost complete

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
-// console.log("this is main.js");
+/*********************************** VISITORS CHART   ***********************************/
 
-var chart = document.querySelector("#visitorsChart");
-
+var chartVisitor = document.querySelector("#ChartVisitors");
 // console.log(chart);
-
-var chartContent = new Chart(chart, {
+var chartContentVisitor = new Chart(chartVisitor, {
 	type: "line",
 	data:{
 		labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"],
@@ -29,35 +27,161 @@ var chartContent = new Chart(chart, {
     }
 });
 
-// var chartContent = new Chart(chart, {
-//     type: "line",
-//     data:{
-//         labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-//         datasets: [{
-//             label: 'DK',
-//             data: [12, 19, 13, 15, 20, 13],
-//             fill: false,
-//             lineTension: 0,
-//             borderColor: 'rgba(255, 159, 64, .8)',
-//             borderWidth: 4
-//         }, {
-//             label: 'SE',
-//             data: [65, 45, 50, 38, 66, 56],
-//             fill: false,
-//             lineTension: 0,
-//             borderColor: 'rgba(200, 108, 132, .8)',
-//             borderWidth: 4
-//         }]
-//     },
-//     options: {
-//         // showLines: false,
-//         scales: {
-//             yAxes: [{
-//                 ticks: {
-//                     beginAtZero: true
-//                 }
-//             }]
-//         }
-//     }
+/*********************************** CLICK RATE CHART   ***********************************/
+
+var chartClickRate = document.querySelector("#ChartClickRate");
+// console.log(chart);
+var chartContentClickRate = new Chart(chartClickRate, {
+    type: "line",
+    data:{
+        labels: ["Age", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"],
+        datasets: [{
+            label: '# Click Rate %',
+            data: [41, 22, 10, 56, 24, 54, 17, 45, 65, 54, 43, 33],
+            fill: false,
+            lineTension: 0.5,
+            borderColor: 'blue',
+            borderWidth: 2
+        }]
+    },
+        options: {
+        // showLines: false,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+/*********************************** AVG SESSION CHART   ***********************************/
+
+var chartSession = document.querySelector("#ChartSession");
+// console.log(chart);
+var chartContentSession = new Chart(chartSession, {
+    type: "line",
+    data:{
+        labels: ["Age", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"],
+        datasets: [{
+            label: '# Avg Session (min)',
+            data: [41, 54, 76, 66, 14, 54, 45, 32, 10, 67, 32, 44],
+            fill: false,
+            lineTension: 0.5,
+            borderColor: 'green',
+            borderWidth: 2
+        }]
+    },
+        options: {
+        // showLines: false,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+/*********************************** BOUNCE RATE CHART   ***********************************/
+
+var chartBounceRate = document.querySelector("#ChartBounceRate");
+// console.log(chart);
+var chartContentClickRate = new Chart(chartBounceRate, {
+    type: "line",
+    data:{
+        labels: ["Age", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"],
+        datasets: [{
+            label: '# Bounce Rate %',
+            data: [70, 50, 110, 56, 67, 54, 32, 65, 89, 32, 87, 44],
+            fill: false,
+            lineTension: 0.5,
+            borderColor: 'purple',
+            borderWidth: 2
+        }]
+    },
+        options: {
+        // showLines: false,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+/*********************************** USER INFO CHART   ***********************************/
+
+var chartUserInfo = document.querySelector("#ChartUserInfo");
+// console.log(chart);
+
+var chartContentUserInfo = new Chart(ChartUserInfo, {
+    type: 'bar',
+    data: {
+        labels: ["Female", "Male", "Other"],
+        datasets: [{
+            label: '# User Info',
+            data: [40, 55, 5],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+
+/*********************************** DISPLAY CHARTS  ***********************************/
+
+var abtnShowChart = document.getElementsByClassName("btnShowChart");
+
+for (var i = 0; i < abtnShowChart.length; i++) {
+    console.log("xx");
+    abtnShowChart[i].addEventListener("click",function(){
+        console.log("Chart btn has been clicked");
+        // HIDE ALL GRAFS
+            var aChart = document.getElementsByClassName( "cChart" );
+            for(var j = 0; j < aChart.length; j++)
+            {
+                console.log("x");
+                aChart[j].classList.remove("displayChart");
+            }
+
+            var sDataAttibute = this.getAttribute("data-showThisChart");
+            // pageOne or pageTwo
+            console.log(sDataAttibute);
+            document.getElementById(sDataAttibute).classList.add("displayChart");
+
+    });
+
+}
+
+
+
 
 
